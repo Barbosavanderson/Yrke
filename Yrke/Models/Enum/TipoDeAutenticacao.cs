ae4e1e4910144f530821ba5736de4f87c979d666
barbosa <barbosavanderson@hotmail.com>
2026-04-14 11:45:07 -0300

    feat: atualizar projeto

--- Yrke/Models/Enum/TipoDeAutenticacao.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Yrke.Models.Enum
-{
-    public enum TipoDeAutenticacao
-    {
-        local = 1, // Email e senha
-        Google = 2 // Autenticação via Google
-    }
-}
--- Yrke/ViewModels/PerfilViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Yrke.ViewModels
-{
-    public class PerfilViewModel
-    {
-        public string Nome { get; set; }
-        public string Funcao { get; set; }
-        public string Email { get; set; }
-    }
-}
