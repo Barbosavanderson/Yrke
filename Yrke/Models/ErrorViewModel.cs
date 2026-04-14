2fd826254365f005bdbabfa17ab3a54e311f0110
barbosa <barbosavanderson@hotmail.com>
2026-04-14 01:50:21 -0300

    ajuste footer + perfil com foto

--- Yrke/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Yrke.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
